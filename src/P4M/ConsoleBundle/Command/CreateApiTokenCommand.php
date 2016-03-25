ac9ecc36111e69f4132a0799823f5a11be320be5
Mohr Jeremie <brynnlow@gmail.com>
2016-03-25 09:58:27 +0100

    préparation de https://github.com/FriendsOfSymfony/FOSRestBundle https://github.com/FriendsOfSymfony/FOSOAuthServerBundle https://github.com/schmittjoh/JMSSerializerBundle https://github.com/nelmio/NelmioApiDocBundle

---
+++ src/P4M/ConsoleBundle/Command/CreateApiTokenCommand.php
@@ -0,0 +1,29 @@
+<?php
+namespace P4M\ConsoleBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateApiTokenCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('api:generate:token')
+        ->setDescription('Génère un client_id et un token pour l\'api')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $clientManager = $this->getContainer()->get('fos_oauth_server.client_manager.default');
+        $client = $clientManager->createClient();
+        $client->setAllowedGrantTypes(array('password'));
+        $reponse = $clientManager->updateClient($client);
+        $output->writeln('Le client et le token on été générer pour l\'api');
+
+
+    }
+}
