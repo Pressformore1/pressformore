ca3e67512c50dd2319b79888d8c3ccf9dd7598af
Mohr Jeremie <brynnlow@gmail.com>
2016-03-31 10:49:26 +0200

    ajout de fonctionnalité a l'api : addPost

--- src/P4M/APIBundle/Controller/DefaultController.php
+++ src/P4M/APIBundle/Controller/DefaultController.php
@@ -42,49 +42,6 @@ class DefaultController extends FOSRestController
 
     /**
      * @ApiDoc(
-     *     resource=True,
-     *     description="add a post"
-     * )
-     */
-    public function getAddPostAction(){
-        $em = $this->getDoctrine()->getManager();
-        $userUtils = $this->get('p4mCore.post_utils');
-        $request = $this->get('request');
-
-        $post = new Post();
-        $form = $this->get('form.factory')->create(new PostType(), $post);
-        $form->remove('blogPost')
-            ->remove('anchors');
-        if(!empty($url = $request->query->get('url'))){
-            $this->response['url']= $url;
-            $postRepo = $em->getRepository('P4MCoreBundle:Post');
-            $langueRepo = $em->getRepository('P4MCoreBundle:Lang');
-
-            $post = $postRepo->findOneBySourceUrl($url);
-
-            if(null === $post){
-                $metas = $userUtils->grabMetas($url);
-            }
-            $this->response['metas'] = $metas;
-            $this->response['form'] = $form;
-
-            $view = $this->view($this->response);
-            return $this->handleView($view);
-        }
-
-
-
-        $response = [
-            'message' => '',
-            'status_codes' => '',
-            'form' => $form,
-        ];
-        $view = $this->view($response);
-        return $this->handleView($view);
-    }
-
-    /**
-     * @ApiDoc(
      *  method="GET",
      *  resource=true,
      *  description="Load a wallet",
---
+++ src/P4M/APIBundle/Controller/ListController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace P4M\APIBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class ListController extends FOSRestController
+{
+    /**
+     * @ApiDoc(
+     *     resource=true,
+     *     description="Get list of category"
+     * )
+     */
+    public function getListCategoryAction()
+    {
+        $categorys = $this->getDoctrine()->getManager()->getRepository('P4MCoreBundle:Category')->findAll();
+
+        $response = $this->view($categorys);
+        return $this->handleView($response);
+    }
+    /**
+     * @ApiDoc(
+     *     resource=true,
+     *     description="Get list of Type"
+     * )
+     */
+    public function getListTypeAction(){
+        $types = $this->getDoctrine()->getManager()->getRepository('P4MCoreBundle:PostType')->findAll();
+
+        $response = $this->view($types);
+        return $this->handleView($response);
+    }
+
+    /**
+     * @ApiDoc(
+     *     resource=true,
+     *     description="Get list of Country"
+     * )
+     */
+    public function getListCountryAction(){
+        $countrys = $this->getDoctrine()->getManager()->getRepository('P4MUserBundle:Country')->findAll();
+
+        $response = $this->view($countrys);
+        return $this->handleView($response);
+    }
+}
