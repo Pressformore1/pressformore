700ebe0088734b84c6f7080bb8588c3b9e540803
Mohr Jeremie <brynnlow@gmail.com>
2016-04-12 14:19:19 +0200

    Header Listener

--- src/P4M/APIBundle/Listener/HeaderListener.php
+++ src/P4M/APIBundle/Listener/HeaderListener.php
@@ -10,7 +10,7 @@ class HeaderListener
     {
         $responseHeaders = $event->getResponse()->headers;
 
-        $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
+        $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept, Authorization');
         $responseHeaders->set('Access-Control-Allow-Origin', '*');
         $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
     }
